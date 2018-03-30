162c54fdf5780f185b5df771098dbf961a453616
safaahusin <safaahusin@yahoo.com>
2018-03-30 22:50:07 +0200

    add skyBox

---
+++ monsters-fly-game/Assets/Scripts/wall.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wall : MonoBehaviour {
+
+    public int x = 10;
+    public int y = 0;
+    
+    public int z = 0;
+    float thing = 6f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
