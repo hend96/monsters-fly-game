5300ecda6bae89e694dcbf0a960a2cd9ab479136
safaahusin <safaahusin@yahoo.com>
2018-04-29 01:17:08 +0200

    remove project in project
    we have problem in github ,, remove some fail object to solve problem

--- monsters-fly-game/Assets/Scripts/wall.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class wall : MonoBehaviour {
-
-    public static List<GameObject> WallMeshReder = new List<GameObject>();
-}
--- monsters-fly-game/Assets/Scripts/win.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class win : MonoBehaviour {
-
-    public static int value=2;
-
-}
